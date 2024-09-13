e54a4827b339bae60a257f7df6c4a0b33472d235
Suriya Kalivardhan <suriyak@microsoft.comn>
2024-09-13 12:19:02 -0700

    Most recent changes

--- azure/aca/scripts/prod/6_deploy_for_project.sh
+++ azure/aca/scripts/prod/6_deploy_for_project.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-acaenvid=/subscriptions/921496dc-987f-410f-bd57-426eb2611356/resourceGroups/suriyak-customer3-chobov2/providers/Microsoft.App/managedEnvironments/suriyak-customer3-acaenv-4
+acaenvid=/subscriptions/921496dc-987f-410f-bd57-426eb2611356/resourceGroups/suriyak-customer3-chobov3/providers/Microsoft.App/managedEnvironments/suriyak-customer3-acaenv-2
+internalingressip=10.1.0.200
 customerumi=/subscriptions/ea4faa5b-5e44-4236-91f6-5483d5b17d14/resourcegroups/suriyak-customer3/providers/Microsoft.ManagedIdentity/userAssignedIdentities/suriyak0customer3-umi
 
 location='eastus2euap'
 commonHoBoSubscription='921496dc-987f-410f-bd57-426eb2611356'
 
-name=$(cut -d'/' -f9 <<<$acaenvid)-4
+name=$(cut -d'/' -f9 <<<$acaenvid)-0
 az group deployment create -f forproject.json  --parameters name=$name identity=$customerumi envid=$acaenvid location=$location
+fqdn=$(az containerapp show -n $name  --query properties.configuration.ingress.fqdn  -o tsv)
+echo internalingressip=$internalingressip
+echo fqdn=$fqdn
+
+#Run these in Platform Multitenant Subnet
+#internalingressip=10.1.0.82
+#fqdn=suriyak-customer3-acaenv-1-0.icywater-7ee6e26f.eastus2euap.azurecontainerapps.io
+#curl $internalingressip/getips -H "HOST: $fqdn"
+#curl $internalingressip/gettoken/$customerumiclientid -H "HOST: $fqdn"
+#curl $internalingressip/getembeddings/$customerumiclientid/$customerstoragename/$customercontainer/$customerblob -H "HOST: $fqdn"
+
 
 #Run these inside the ACA instance for sidecar access
 #echo $IDENTITY_ENDPOINT
